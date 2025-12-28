5db07f1448aec78fcd9fb57b3a3f614380601a97
Abd-erahmane <bfabderahman@gmail.com>
2025-12-28 23:18:23 +0100

    Mise à jour du site

--- db.js
+++ db.js
@@ -1,19 +1,17 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "mobilis_auth",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
     console.error("❌ Erreur connexion MySQL :", err.message);
   } else {
     console.log("✅ MySQL connecté");
   }
 });
-
-module.exports = db;
